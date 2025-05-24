b698392731f94a3e8538ac95bc5efd6a75804f3c
Pineapple <caanneerr01@gmail.com>
2025-05-24 21:58:10 +0400

    10 ,bktn

--- num_10/script.js
+++ num_10/script.js
@@ -1,14 +1,37 @@
-fetch("data.json")
+fetch("data_2.json")
   .then((response) => response.json())
   .then((data) => {
-    const columns = document.getElementsByName("columns");
+    const columns = document.getElementById("columns");
 
-    [("1", "2", "3")].forEach((element) => {
-      console.log(data[element]);
+    data.forEach((element) => {
+      // console.log(element["group_title"]);
 
       const column = document.createElement("div");
       column.className = "column";
 
+      const groupTitle = document.createElement("h2");
+      groupTitle.textContent = element["group_title"];
+
+      column.appendChild(groupTitle);
+
+      const article = document.createElement("article");
+      article.className = "card";
+
+      element["cards"].forEach((card) => {
+        const title = document.createElement("h1");
+        title.textContent = card["title"];
+        article.appendChild(title);
+
+        const content = document.createElement("p");
+        content.textContent = card["content"];
+        article.appendChild(content);
+
+        const date = document.createElement("p");
+        date.textContent = card["date"];
+        date.className = "date";
+        article.appendChild(date);
+      });
+      column.appendChild(article);
       columns.appendChild(column);
     });
   })
